2224e85508a268933df22276dc6af9091795aa05
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-06 12:16:21 -0100

    style the header and body

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -6,8 +6,15 @@ const Login: React.FunctionComponent = () => {
     <div className="login-container">
       <div className="login-box">
         <div className="login-box__logo"></div>
-        <div className="login-box__heading"></div>
-        <div className="login-box__body"></div>
+        <div className="login-box__heading">
+          <p>Join thousands of learners from</p>
+          <p>around the world </p>
+        </div>
+        <div className="login-box__body">
+          <p>Master web development by making real-life</p>
+          <p>projects. There are multiple paths for you to </p>
+          <p>choose</p>
+        </div>
         <form className="login-box__form"></form>
         <div className="login-box__socials"></div>
         <div className="login-box__link"></div>
