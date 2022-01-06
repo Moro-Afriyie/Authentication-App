fb596c93c72f4510e93215b62876d8da85b27300
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-06 13:23:25 -0100

    add the logo

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
 import "./Login.scss";
+import logo from "../../assets/devchallenges.svg";
 
 const Login: React.FunctionComponent = () => {
   return (
     <div className="login-container">
       <div className="login-box">
-        <div className="login-box__logo"></div>
+        <div className="login-box__logo">
+          <img src={logo} alt="logo" />
+        </div>
         <div className="login-box__heading">
           <p>Join thousands of learners from</p>
           <p>around the world </p>
