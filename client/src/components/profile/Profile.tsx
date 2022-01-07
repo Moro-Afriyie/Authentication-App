519fdcbd4f01a6a120f8747db1171d209b3a2b4b
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-07 10:29:30 -0100

    add a logic to open and close dropdown menu

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -4,6 +4,12 @@ import logo from "../../assets/devchallenges.svg";
 import avatar from "../../assets/avatar.jpg";
 
 const Profile: React.FunctionComponent = () => {
+  const [menu, setMenu] = React.useState(false);
+
+  const handleShowDropdownMenu = () => {
+    setMenu((prev) => !prev);
+  };
+
   return (
     <div className="profile-container">
       <div className="profile">
@@ -12,31 +18,39 @@ const Profile: React.FunctionComponent = () => {
             <img src={logo} alt="logo" />
           </div>
           <div className="profile__nav-avatar">
-            <div className="profile__nav-avatar-controls">
+            <div
+              className="profile__nav-avatar-controls"
+              onClick={handleShowDropdownMenu}
+            >
               <div className="avatar">
                 <img src={avatar} alt="profile picture" />
               </div>
               <p>Xanthe Neal</p>
-              <i className="fa fa-caret-down" aria-hidden="true"></i>
-              {/* <i className="fa fa-caret-up" aria-hidden="true"></i> */}
+              {menu ? (
+                <i className="fa fa-caret-up" aria-hidden="true"></i>
+              ) : (
+                <i className="fa fa-caret-down" aria-hidden="true"></i>
+              )}
             </div>
-            <div className="drop-down__menu">
-              <ul className="drop-down__menu-links">
-                <li className="link-item profile active">
-                  <span className="material-icons">account_circle</span>
-                  <p>My Profile</p>
-                </li>
-                <li className="link-item group">
-                  <span className="material-icons">group</span>
-                  <p>Group Chat</p>
-                </li>
-                <hr />
-                <li className="link-item logout">
-                  <span className="material-icons">exit_to_app</span>
-                  <p>Logout</p>
-                </li>
-              </ul>
-            </div>
+            {menu && (
+              <div className="drop-down__menu">
+                <ul className="drop-down__menu-links">
+                  <li className="link-item profile active">
+                    <span className="material-icons">account_circle</span>
+                    <p>My Profile</p>
+                  </li>
+                  <li className="link-item group">
+                    <span className="material-icons">group</span>
+                    <p>Group Chat</p>
+                  </li>
+                  <hr />
+                  <li className="link-item logout">
+                    <span className="material-icons">exit_to_app</span>
+                    <p>Logout</p>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
         <div className="profile__info">
