9cdf4cdef62f0bb8ce9ef47e009eeba88be93891
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-07 08:52:07 -0100

    style the profile and add the logo

--- client/src/components/profile/Profile.tsx
+++ client/src/components/profile/Profile.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import "./Profile.scss";
+import logo from "../../assets/devchallenges.svg";
 
 const Profile: React.FunctionComponent = () => {
   return (
     <div className="profile-container">
-      <div className="profile-box">
-        <h1>Profile</h1>
+      <div className="profile">
+        <div className="profile__nav">
+          <div className="profile__nav-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="profile__nav-avatar">
+            <h1>Avatar</h1>
+          </div>
+        </div>
+        <div className="profile__info">
+          <div className="profile__info-header"></div>
+          <div className="profile__info-details"></div>
+        </div>
       </div>
     </div>
   );
