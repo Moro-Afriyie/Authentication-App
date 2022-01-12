888f879f7abced3dadd4d7a3e00c1f9896c4620f
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-12 13:58:25 -0100

    add the profile details

--- client/src/components/profileDetails/ProfileDetails.tsx
+++ client/src/components/profileDetails/ProfileDetails.tsx
@@ -13,10 +13,23 @@ const ProfileDetails: React.FunctionComponent<IProfileDetailsProps> = (
         <p className="bold">Personal info</p>
         <p className="faint">Basic info, like your name and photo</p>
       </div>
-      <div className="profile-details-box__info">
-        <div className="profile-details-box__info-controls"></div>
-        <div className="profile-details-box__info-lists"></div>
-      </div>
+      {/* <div className="profile-details-box__info"> */}
+      <ul className="profile-details-box__info">
+        <li className="profile-details-box__info-controls">
+          <div className="profile">
+            <p className="ligt-bold">Profile</p>
+            <p className="light">Some info may be visible to other people</p>
+          </div>
+          <div className="edit">edit</div>
+        </li>
+        <li className="photo">phone</li>
+        <li className="name">name</li>
+        <li className="bio">bio</li>
+        <li className="phone-number">number</li>
+        <li className="email">email</li>
+        <li className="password">password</li>
+      </ul>
+      {/* </div> */}
     </div>
   );
 };
