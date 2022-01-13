1abe849f6898d9099ccb040926aeb4b5002258a2
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-13 12:06:25 -0100

    style the edit profile form

--- client/src/components/editProfile/EditProfile.tsx
+++ client/src/components/editProfile/EditProfile.tsx
@@ -8,7 +8,10 @@ interface IEditProfileProps {}
 const EditProfile: React.FunctionComponent<IEditProfileProps> = (props) => {
   return (
     <div className="edit-profile-details-box">
-      <div className="back">back</div>
+      <div className="back">
+        <span className="material-icons">arrow_back_ios_new</span>
+        <p>back</p>
+      </div>
       <div className="edit-details-info">
         <div className="edit-details-info__header">
           <p className="bold">Change Info</p>
@@ -22,12 +25,61 @@ const EditProfile: React.FunctionComponent<IEditProfileProps> = (props) => {
                   className="photo-icon"
                   style={{ backgroundImage: `url(${avatar})` }}
                 >
-                  <span className="material-icons">photo_camera</span>
+                  <div className="icon">
+                    <span className="material-icons">photo_camera</span>
+                  </div>
                 </div>
                 <p>CHANGE PHOTO</p>
               </label>
               <input type="file" name="photo" id="photo" />
             </div>
+            <div className="form-control">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Enter your name..."
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="bio">Bio</label>
+              <textarea
+                name="bio"
+                id="bio"
+                placeholder="Enter your bio..."
+              ></textarea>
+            </div>
+            <div className="form-control">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                placeholder="Enter your phone..."
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email..."
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your password..."
+              />
+            </div>
+            <button type="submit" className="submit">
+              Save
+            </button>
           </form>
         </div>
       </div>
