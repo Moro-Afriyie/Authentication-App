eefddebf1833c38a542feb3893e155cc8737c6c6
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-13 10:26:59 -0100

    style the edit photo

--- client/src/components/editProfile/EditProfile.tsx
+++ client/src/components/editProfile/EditProfile.tsx
@@ -13,7 +13,18 @@ const EditProfile: React.FunctionComponent<IEditProfileProps> = (props) => {
           <p className="bold">Change Info</p>
           <p className="light">Changes will be reflected to every services</p>
         </div>
-        <div className="edit-details-info__form"></div>
+        <div className="edit-details-info__form">
+          <form>
+            <div className="change-photo">
+              <label htmlFor="photo">
+                <div className="photo-icon">
+                  <span className="material-icons">photo_camera</span>
+                </div>
+              </label>
+              <input type="file" name="photo" id="photo" />
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   );
