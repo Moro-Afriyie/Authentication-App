73b18bf3de4ac73acf593fe3859a37ab12f3a794
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-14 08:19:07 -0100

    set up the routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,26 @@ import "./App.scss";
 import Login from "./components/login/Login";
 import Profile from "./components/profile/Profile";
 import SignUp from "./components/signup/Signup";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProfileDetails from "./components/profileDetails/ProfileDetails";
+import EditProfile from "./components/editProfile/EditProfile";
 
 function App() {
   return (
     <div className="container">
-      <Login />
-      {/* <SignUp /> */}
-      {/* <Profile /> */}
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} />
+          <Route path="profile" element={<Profile />}>
+            <Route path=":username" element={<ProfileDetails />} />
+            <Route path="settings/profile" element={<EditProfile />} />
+          </Route>
+        </Route>
+        {/* <Login /> */}
+        {/* <SignUp /> */}
+        {/* <Profile /> */}
+      </Routes>
     </div>
   );
 }
