c3d4b775ef64f87af1995236967a7a48be64eca9
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2022-01-15 10:55:00 -0100

    add jsx to the signUp component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,16 +10,17 @@ import EditProfile from "./components/editProfile/EditProfile";
 function App() {
   return (
     <div className="container">
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Profile />}>
-          {/* <Route path=":username" element={<ProfileDetails />} /> */}
+          {/* <Route path=":username" element={<ProfileDetails />} /> 
           <Route index element={<ProfileDetails />} />
           <Route path="/username" element={<ProfileDetails />} />
           <Route path="settings/profile" element={<EditProfile />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<SignUp />} />
-      </Routes>
+      </Routes> */}
+      <SignUp />
     </div>
   );
 }
--- client/src/components/signup/Signup.tsx
+++ client/src/components/signup/Signup.tsx
@@ -1,8 +1,63 @@
 import * as React from "react";
 import "./SignUp.scss";
+import logo from "../../assets/devchallenges.svg";
 
 const SignUp: React.FunctionComponent = () => {
-  return <h1>SignUp Component</h1>;
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <div className="login-box__logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="login-box__heading">
+          <p>Sign Up</p>
+        </div>
+        <form className="login-box__form">
+          <div className="login-box__form-control">
+            <i className="fa fa-envelope fa-lg" aria-hidden="true"></i>
+            <input type="email" name="email" id="email" placeholder="Email" />
+          </div>
+          <div className="login-box__form-control">
+            <i className="fa fa-lock fa-2x" aria-hidden="true"></i>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+          </div>
+          <button className="login-box__form-button" type="submit">
+            Sign Up
+          </button>
+        </form>
+        <div className="login-box__socials">
+          <p>or continue with these social profile</p>
+          <div className="login-box__social-icons">
+            <div className="icon">
+              <i className="fa fa-google fa-lg" aria-hidden="true"></i>
+            </div>
+            <div className="icon">
+              <i
+                className="fa fa-facebook-official fa-lg"
+                aria-hidden="true"
+              ></i>
+            </div>
+            <div className="icon">
+              <i className="fa fa-twitter fa-lg" aria-hidden="true"></i>
+            </div>
+            <div className="icon">
+              <i className="fa fa-github fa-lg" aria-hidden="true"></i>
+            </div>
+          </div>
+        </div>
+        <div className="login-box__link">
+          <p>
+            Don&apos;t have an account? <a href="#">SignUp</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
