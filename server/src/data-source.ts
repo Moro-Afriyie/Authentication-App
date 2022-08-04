2e2d567709ab72c189755f24fd28c1585dcc09d1
Moro-Afriyie <ceremoinc99@gmail.com>
2022-08-04 16:30:45 +0000

    setup the database config

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,17 +1,19 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import { User } from './entity/User';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
+import { config as configDotenv } from 'dotenv';
 
+configDotenv();
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
-})
+	type: 'mysql',
+	host: process.env.DB_HOST || 'localhost',
+	port: +(process.env.DB_PORT || '3306'),
+	database: process.env.DB_NAME,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	synchronize: process.env.NODE_ENV !== 'production',
+	logging: process.env.NODE_ENV === 'development',
+	entities: [User],
+	migrations: [],
+	subscribers: [],
+});
