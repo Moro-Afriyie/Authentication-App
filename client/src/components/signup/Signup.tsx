d956540b91037b8c5fe7e5be955c13bd319f8f78
Moro-Afriyie <ceremoinc99@gmail.com>
2022-08-25 11:24:58 +0000

    add the controls for the login form

--- client/src/components/signup/Signup.tsx
+++ client/src/components/signup/Signup.tsx
@@ -4,6 +4,21 @@ import logo from "../../assets/devchallenges.svg";
 import { Link } from "react-router-dom";
 
 const SignUp: React.FunctionComponent = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [userName, setUserName] = React.useState("");
+
+  const handleSocialLogin = (account: string) => {
+    window.open(`http://localhost:8080/auth/${account}`, "_self");
+  };
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("email: ", email);
+    console.log("password: ", password);
+    console.log("userName: ", userName);
+  };
+
   return (
     <div className="login-container">
       <div className="login-box">
@@ -13,19 +28,30 @@ const SignUp: React.FunctionComponent = () => {
         <div className="login-box__heading">
           <p>Sign Up</p>
         </div>
-        <form className="login-box__form">
+        <form className="login-box__form" onSubmit={(e) => handleLogin(e)}>
           <div className="login-box__form-control">
             <i className="fa fa-user-circle-o fa-lg" aria-hidden="true"></i>
             <input
               type="text"
               name="userName"
               id="userName"
+              value={userName}
               placeholder="User Name"
+              required
+              onChange={(e) => setUserName(e.target.value)}
             />
           </div>
           <div className="login-box__form-control">
             <i className="fa fa-envelope fa-lg" aria-hidden="true"></i>
-            <input type="email" name="email" id="email" placeholder="Email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
           <div className="login-box__form-control">
             <i className="fa fa-lock fa-2x" aria-hidden="true"></i>
@@ -33,7 +59,10 @@ const SignUp: React.FunctionComponent = () => {
               type="password"
               name="password"
               id="password"
+              value={password}
+              required
               placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button className="login-box__form-button" type="submit">
@@ -42,20 +71,23 @@ const SignUp: React.FunctionComponent = () => {
         </form>
         <div className="login-box__socials">
           <p>or continue with these social profile</p>
-          <div className="login-box__social-icons">
+          <div
+            className="login-box__social-icons"
+            onClick={() => handleSocialLogin("google")}
+          >
             <div className="icon">
               <i className="fa fa-google fa-lg" aria-hidden="true"></i>
             </div>
-            <div className="icon">
+            <div className="icon" onClick={() => handleSocialLogin("facebook")}>
               <i
                 className="fa fa-facebook-official fa-lg"
                 aria-hidden="true"
               ></i>
             </div>
-            <div className="icon">
+            <div className="icon" onClick={() => handleSocialLogin("twitter")}>
               <i className="fa fa-twitter fa-lg" aria-hidden="true"></i>
             </div>
-            <div className="icon">
+            <div className="icon" onClick={() => handleSocialLogin("github")}>
               <i className="fa fa-github fa-lg" aria-hidden="true"></i>
             </div>
           </div>
