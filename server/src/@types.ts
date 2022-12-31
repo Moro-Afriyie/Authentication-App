de6eb370de7da81abd457228e54e974ee51fc158
Moro-Afriyie <ceremoinc99@gmail.com>
2022-12-31 19:20:59 +0000

    feat:add route to update details

--- server/src/@types.ts
+++ server/src/@types.ts
@@ -1,3 +1,4 @@
+import { User as UserEntity } from './entity/User';
 import { Router } from 'express';
 
 export interface IEndpoint {
@@ -10,4 +11,10 @@ export enum HttpStatusCode {
 	NOT_FOUND = 404,
 	INTERNAL_SERVER = 500,
 	UNAUTHORISED = 401,
+}
+
+declare global {
+	namespace Express {
+		interface User extends UserEntity {}
+	}
 }
--- server/src/api/users.ts
+++ server/src/api/users.ts
@@ -1,6 +1,8 @@
+import { HttpStatusCode } from './../@types';
 import { Router } from 'express';
 import { AppDataSource } from '../data-source';
 import { User } from '../entity/User';
+import { APIError } from '../error';
 
 const router: Router = Router();
 
@@ -9,6 +11,17 @@ export const UserRepository = AppDataSource.getRepository(User);
 router.get('/', async (req, res) => {
 	const users = await UserRepository.find();
 	res.status(200).json({ success: true, data: users });
+});
+
+router.put('/', async (req, res) => {
+	const user = await UserRepository.findBy({ id: req.user.id });
+
+	if (!user) {
+		throw new APIError('NOT FOUND', HttpStatusCode.NOT_FOUND, true, 'User not found');
+	}
+	Object.assign(user, req.params);
+	const updatedUser = await UserRepository.save(user);
+	res.json({ succes: true, user: updatedUser });
 });
 
 router.get('/delete', async (req, res) => {
