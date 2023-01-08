e0e112660843d38a584e5040b43c17c86441e8f9
Moro-Afriyie <ceremoinc99@gmail.com>
2023-01-08 19:55:23 +0000

    feat:create the storage middleware

---
+++ server/src/middlewares/storageMiddleware.ts
@@ -0,0 +1,20 @@
+import multer from 'multer';
+import { v2 as cloudinary } from 'cloudinary';
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
+
+cloudinary.config({
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+});
+
+const storage = new CloudinaryStorage({
+	cloudinary: cloudinary,
+	params: {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		resource_type: 'auto',
+	},
+});
+
+export const storageMiddleware = multer({ storage: storage });
