c256d365256bccb4dac3c8de87dc57ef8eb3f491
Moro-Afriyie <ceremoinc99@gmail.com>
2023-01-08 19:55:04 +0000

    add the validations for the user schema

--- server/src/api/users.ts
+++ server/src/api/users.ts
@@ -9,13 +9,15 @@ import Joi = require('joi');
 const router: Router = Router();
 
 const userSchema = Joi.object({
-	name: Joi.string().min(3).max(30).required(),
+	name: Joi.string().min(3).max(30),
+	bio: Joi.string().max(500),
 	email: Joi.string()
 		.email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
 		.required(),
 	photo: Joi.string()
 		.uri({ scheme: ['http', 'https'] })
 		.allow(''),
+	password: Joi.string().min(5).max(20).required(),
 });
 
 export const UserRepository = AppDataSource.getRepository(User);
