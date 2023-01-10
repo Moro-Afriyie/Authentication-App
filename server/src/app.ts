ce670d70761aefd824518d0e80d69a213cebd6a7
Moro-Afriyie <ceremoinc99@gmail.com>
2023-01-10 11:55:13 +0000

    feat:changed the cors origin

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,7 +16,7 @@ app.use(helmet());
 // setup cross-origin resource header sharing
 app.use(
 	cors({
-		origin: 'http://localhost:3000',
+		origin: '*',
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 		credentials: true,
 	})
